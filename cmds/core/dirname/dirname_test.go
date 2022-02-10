e97db231598c3051b607f37d4b9e93c3338e1b7e
Christopher Meis <christopher.meis@9elements.com>
2022-02-10 12:49:21 +0100

    cmds/core/dirname: * Add rework tests for new layout of dirname
    Signed-off-by: Christopher Meis <christopher.meis@9elements.com>

--- cmds/core/dirname/dirname_test.go
+++ cmds/core/dirname/dirname_test.go
@@ -7,49 +7,45 @@ package main
 import (
 	"bytes"
 	"testing"
-
-	"github.com/u-root/u-root/pkg/testutil"
 )
-
-type test struct {
-	args []string
-	out  string
-	err  string
-}
-
-var dirnameTests = []test{
-	// For no args it seems we have to print an error.
-	// It should be missing operand[s] but that's not the standard.
-	{args: []string{}, err: "dirname: missing operand\n"},
-	{args: []string{""}, out: ".\n"},
-	{args: []string{"/this/that"}, out: "/this\n"},
-	{args: []string{"/this/that", "/other"}, out: "/this\n/\n"},
-	{args: []string{"/this/that", "/other thing/space"}, out: "/this\n/other thing\n"},
-}
 
 func TestDirName(t *testing.T) {
 	// Table-driven testing
-	for _, tt := range dirnameTests {
-		c := testutil.Command(t, tt.args...)
-		stdout, stderr := &bytes.Buffer{}, &bytes.Buffer{}
-		c.Stdout, c.Stderr = stdout, stderr
-		err := c.Run()
-		if err != nil && tt.err == "" {
-			t.Errorf("Test %v: got %q, want nil", tt.args, err)
-			continue
-		}
-
-		t.Logf("RUN: %v: got %q, %q", tt.args, stdout, stderr)
-		if stdout.String() != tt.out {
-			t.Errorf("%v: stdout got %q, wants %q", tt.args, stdout.String(), tt.out)
-		}
-		if err != nil && stderr.String() != "" && stderr.String()[len("yyyy/mm/dd hh:mm:ss "):] != tt.err {
-			t.Errorf("%v: stderr got %q, wants %q", tt.args, stderr.String(), tt.err)
-		}
-
+	for _, tt := range []struct {
+		name string
+		args []string
+		out  string
+	}{
+		{
+			name: "Usage",
+			args: []string{},
+		},
+		{
+			name: "EmptyArgs",
+			args: []string{""},
+			out:  ".\n",
+		},
+		{
+			name: "/this/that",
+			args: []string{"/this/that"},
+			out:  "/this\n",
+		},
+		{
+			name: "/this/that_/other",
+			args: []string{"/this/that", "/other"},
+			out:  "/this\n/\n",
+		},
+		{
+			name: "/this/that_/other thing/space",
+			args: []string{"/this/that", "/other thing/space"},
+			out:  "/this\n/other thing\n",
+		},
+	} {
+		t.Run(tt.name, func(t *testing.T) {
+			var buf bytes.Buffer
+			if err := runDirname(&buf, tt.args); err != nil {
+				t.Errorf("runDirname(%v, &buf)=%q, want nil", tt.args, err)
+			}
+		})
 	}
-}
-
-func TestMain(m *testing.M) {
-	testutil.Run(m, main)
 }
